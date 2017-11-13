52e9e5936476965b8947d2c873d5dd2086ec5e4f
dos Santos, Diego <diego.dos.santos@sap.com>
2017-11-13 18:47:34 -0200

    no message

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -3,8 +3,20 @@ sap.ui.define([
 ], function (Controller) {
   "use strict";
   return Controller.extend("com.diegodossantos95.MMPSFrontend.controller.Master", {
-      entityListFactory: function(){
-          
-      }
+      
+    onInit: function () {
+      this._initRoute();
+    },
+      
+    // private function
+      
+    _initRoute: function () {
+      this.getRouter().getRoute('SplitApp').attachPatternMatched(this._routeMatch, this);
+    },
+      
+    _routeMatch: function (oEvent) {
+      var sEntity = oEvent.getParameter("arguments").entity;
+      console.log(sEntity);
+    }
   });
 });
